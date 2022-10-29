3ad599fbb9416dcbec4f665011317b43a1d0f066
Sveta Andreeva <89111378147v@yandex.com>
2022-10-29 09:05:39 +0300

    pars Json

--- src/test/java/org/example/JsonParsing.java
+++ src/test/java/org/example/JsonParsing.java
@@ -4,27 +4,19 @@ import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
 
 public class JsonParsing {
 
     @Test
     public void testRestAssured() {
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "John");
-
         JsonPath response = RestAssured
                 .given()
-                .queryParams(params)
-                .get("https://playground.learnqa.ru/api/hello")
+                .get("https://playground.learnqa.ru/api/get_json_homework")
                 .jsonPath();
 
-        String name = response.get("answer");
-        if (name == null) {
-            System.out.println("The key 'answer2' is absent");
-        } else {
-            System.out.println(name);
-        }
+        ArrayList answer = response.get("messages");
+        System.out.println(answer.get(1));
     }
 }
+
